fa0a89e12927eec1fa429492b56981edfd7c0b67
RYRY1002 <ryry1002yt@gmail.com>
2020-12-02 14:12:24 +1100

    Removed Biomes O' Plenty materials & gold block from "GetMaterialIDs"
    Don't know why the Biomes O' Plenty were in there in the first place.
    Removed Gold Block because it is not featured in the video

--- shaders/include/GetMaterialIDs.glsl
+++ shaders/include/GetMaterialIDs.glsl
@@ -9,23 +9,13 @@ float GetMaterialIDs() {						//Gather materials
 		case 83:								//Sugar Cane
 		case 106:								//Vine
 		case 175:								//Double Tall Grass
-		case 1920:								//Biomes O Plenty: Thorns, barley
-		case 1921:								//Biomes O Plenty: Sunflower
-		case 1925:								//Biomes O Plenty: Medium Grass
 					materialID = 2.0; break;	//Translucent blocks
 		case 18:								//Leaves
-		case 161:								//Biomes O Plenty: Giant Flower Leaves
-		case 1923:								//Biomes O Plenty: Leaves
-		case 1924:								//Biomes O Plenty: Leaves
-		case 1926:								//Biomes O Plenty: Leaves
-		case 1936:								//Biomes O Plenty: Giant Flower Leaves
-		case 1962:								//Biomes O Plenty: Leaves
 					materialID = 3.0; break;	//Leave
 		case 8:
 		case 9:
 					materialID = 4.0; break;	//Water
 		case 79:	materialID = 5.0; break;	//Ice
-		case 41:	materialID = 6.0; break;	//Gold block
 		case 50:	materialID = 7.0; break;	//Torch
 		case 10:
 		case 11:	materialID = 8.0; break;	//Lava
