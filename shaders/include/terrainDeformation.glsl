6a4ccd42945197a83d2c3c3868578a9542e7bae7
RYRY1002 <ryry1002yt@gmail.com>
2020-12-16 08:18:37 +1100

    Fixed bugs

--- shaders/include/terrainDeformation.glsl
+++ shaders/include/terrainDeformation.glsl
@@ -1,11 +1,11 @@
 // Deformation Controller
 float deformationController = 1.0;
-deformationController -= 1.0 * sinpowslow(clamp01(track, 2283.5, 123.5), 3.0);
+deformationController -= 1.0 * sinpowfast(clamp01(track, 2283.5, 492.5), 3.0);
 deformationController += 1.0 * sinpowfast(clamp01(track, 3528.5, 157.5), 3.0);
 deformationController -= 1.0 * sinpowsmooth(clamp01(track, 13207.5, 492.5), 3.0);
 deformationController += 1.0 * sinpowsmooth(clamp01(track, 14675.5, 820.5), 3.0);
-deformationController -= 1.0 * sinpowfast(clamp01(track, 20503.5, 100.5), 3.0);
-deformationController += 1.0 * sinpowfast(clamp01(track, 21827.0, 22319.5), 3.0);
+deformationController -= 1.0 * sinpowfast(clamp01(track, 20503.5, 250.5), 3.0);
+deformationController += 1.0 * sinpowfast(clamp01(track, 21827.0, 492.5), 3.0);
 
 // Standard Deformation
 om = intensity * sin(Distance * sin(time * speed / 256.0) / 5000);
@@ -13,7 +13,7 @@ rotate(position.yz, om / 1.5 * deformationController);
 
 // P1 Custom Deformation
 float customDeformationP1 = 0.0;
-customDeformationP1  = 1.0 * sinpowfast(clamp01(track, 2283.5, 246.5), 3.0);
+customDeformationP1  = 1.0 * sinpowfast(clamp01(track, 2283.5, 492.5), 3.0);
 customDeformationP1 -= 1.0 * sinpowfast(clamp01(track, 3528.5, 157.5), 3.0);
 position.y -= 3.25 * customDeformationP1;
 rotate(position.yz, position.x * customDeformationP1 * 0.05);
