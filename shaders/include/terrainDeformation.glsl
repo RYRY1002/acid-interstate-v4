24727d578ff76b10ce48d37e8cd896395e2b4cc9
RYRY1002 <ryry1002yt@gmail.com>
2020-12-15 21:49:43 +1100

    Added more Terrain Deformation
    Video is now in a somewhat finished state.
    (As in finished enough that I could record it and publish it to YouTube)

--- shaders/include/terrainDeformation.glsl
+++ shaders/include/terrainDeformation.glsl
@@ -4,6 +4,8 @@ deformationController -= 1.0 * sinpowslow(clamp01(track, 2283.5, 123.5), 3.0);
 deformationController += 1.0 * sinpowfast(clamp01(track, 3528.5, 157.5), 3.0);
 deformationController -= 1.0 * sinpowsmooth(clamp01(track, 13207.5, 492.5), 3.0);
 deformationController += 1.0 * sinpowsmooth(clamp01(track, 14675.5, 820.5), 3.0);
+deformationController -= 1.0 * sinpowfast(clamp01(track, 20503.5, 100.5), 3.0);
+deformationController += 1.0 * sinpowfast(clamp01(track, 21827.0, 22319.5), 3.0);
 
 // Standard Deformation
 om = intensity * sin(Distance * sin(time * speed / 256.0) / 5000);
@@ -11,7 +13,7 @@ rotate(position.yz, om / 1.5 * deformationController);
 
 // P1 Custom Deformation
 float customDeformationP1 = 0.0;
-customDeformationP1  = 1.0 * sinpowslow(clamp01(track, 2283.5, 138.5), 3.0);
+customDeformationP1  = 1.0 * sinpowfast(clamp01(track, 2283.5, 246.5), 3.0);
 customDeformationP1 -= 1.0 * sinpowfast(clamp01(track, 3528.5, 157.5), 3.0);
 position.y -= 3.25 * customDeformationP1;
 rotate(position.yz, position.x * customDeformationP1 * 0.05);
@@ -19,15 +21,23 @@ position.y += 3.25 * customDeformationP1;
 
 // P3 Custom Deformation
 float customDeformationP3 = 0.0;
-customDeformationP3  = 1.0 * sinpowsmooth(clamp01(track, 10314.5, 492.5), 3.0);
-customDeformationP3 -= 1.0 * sinpowsmooth(clamp01(track, 12930.5, 257.5), 3.0);
+customDeformationP3  = 1.0 * sinpowfast(clamp01(track, 10314.5, 615.5), 3.0);
+customDeformationP3 -= 1.0 * sinpowslow(clamp01(track, 12930.5, 257.5), 3.0);
 rotate(position.xz, position.x / 200.0 * customDeformationP3);
 rotate(position.xz, radians(-45.0) * customDeformationP3);
 
 // P4 Custom Deformation
 float customDeformationP4 = 0.0;
-customDeformationP4  = 1.0 * sinpowslow(clamp01(track, 13207.5, 492.5), 3.0);
-customDeformationP4 -= 1.0 * sinpowfast(clamp01(track, 14675.5, 820.5), 3.0);
+customDeformationP4  = 1.0 * sinpowsmooth(clamp01(track, 13207.5, 492.5), 3.0);
+customDeformationP4 -= 1.0 * sinpowsmooth(clamp01(track, 14675.5, 820.5), 3.0);
 position.y -= 3.25 * customDeformationP4;
 rotate(position.yz, position.x * customDeformationP4 * 0.05);
 position.y += 3.25 * customDeformationP4;
+
+// P5 Custom Deformation
+float customDeformationP5 = 0.0;
+customDeformationP5  = 1.0 * sinpowfast(clamp01(track, 20503.5, 250.5), 3.0);
+customDeformationP5 -= 1.0 * sinpowfast(clamp01(track, 21827.0, 492.5), 3.0);
+rotate(position.xy, abs(position.x) / 500.0 * customDeformationP5);
+rotate(position.zy, abs(position.z) / 75.0 * customDeformationP5);
+rotate(position.zx, position.x / 200.0 * customDeformationP5);
